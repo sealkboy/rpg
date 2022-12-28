0cfb93957ca06c806e95ebf79341295c6f763f56
pilpod <giacomo.corso@gmail.com>
2022-12-28 17:31:43 +0100

    init repo

---
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -0,0 +1,5 @@
+package com.factoriaf5.kata;
+
+public class Character {
+    
+}
---
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -0,0 +1,14 @@
+package com.factoriaf5.kata;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CharacterTest {
+    
+    @Test
+    public void ExampleTest() {
+        assertEquals(2, 2);
+    }
+
+}
